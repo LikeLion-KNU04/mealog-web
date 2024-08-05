21ddae22057add5e783cacfed195a17dfc2c6f07
MOJAN <sjs732@naver.com>
2024-08-05 22:18:08 +0900

    feat: 상품 카드 추가

---
+++ apps/web/src/components/ProductCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductCardProps extends React.HTMLAttributes<HTMLDivElement> {
+    productImage: string;
+    productId: number;
+    productName: string;
+    productPrice: number;
+    productDetail: string;
+}
+
+export default function ProductCard({
+    productImage,
+    productId,
+    productName,
+    productPrice,
+    productDetail,
+    ...props
+}: ProductCardProps) {
+    return (
+        <div {...props} className="flex flex-col w-full">
+            <Link href={"/market/" + productId}>
+                <Image
+                    className="w-full"
+                    src="https://picsum.photos/200/200"
+                    alt="product Image"
+                    width={200}
+                    height={200}
+                />
+            </Link>
+            <div className="text-base text-gray-500">
+                {productDetail}
+            </div>
+            <Link href={"/market/" + productId}>
+                <div className="text-lg">
+                    {productName}
+                </div>
+            </Link>
+            <div className="text-xl">
+                <b>{productPrice.toLocaleString('ko-KR')}</b>원
+            </div>
+        </div>
+    )
+}
